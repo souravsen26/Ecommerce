bde1b0fae8ba9ed68035177eeb5869b9f0de585b
Sourav <sousen26@gmail.com>
2025-07-13 16:56:36 +0530

    Refactored Eureka Client MS

--- Enabling_Group_Miroservices/Service_Discovery_Client_Microservice/src/main/java/com/ecommerce/main/EurekaClientMS.java
+++ Enabling_Group_Miroservices/Service_Discovery_Client_Microservice/src/main/java/com/ecommerce/main/EurekaClientMS.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EcommerceServiceApplication {
+public class EurekaClientMS {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceServiceApplication.class, args);
+		SpringApplication.run(EurekaClientMS.class, args);
 	}
 
 }
